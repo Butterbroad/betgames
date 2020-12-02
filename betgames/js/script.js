267a139b8a5d059e6ef87330df6fc1aedda58365
Butterbroad <pyatochevskiy@gmail.com>
2020-12-02 19:49:03 +0300

    started adaptive screen1

--- betgames/js/script.js
+++ betgames/js/script.js
@@ -1,23 +1,23 @@
-const pageSlider = new Swiper('.page', {
-  wrapperClass: 'page__wrapper',
-  slideClass: 'page__screen',
-  direction: 'vertical',
-  slidesPerView: 'auto',
+// const pageSlider = new Swiper('.page', {
+//   wrapperClass: 'page__wrapper',
+//   slideClass: 'page__screen',
+//   direction: 'vertical',
+//   slidesPerView: 'auto',
 
-  keyboard: {
-    enabled: true,
-    onlyInViewport: true,
-    pageUpDown: true
-  },
-  mousewheel: {
-    sensitivity: 1,
-  },
+//   keyboard: {
+//     enabled: true,
+//     onlyInViewport: true,
+//     pageUpDown: true
+//   },
+//   mousewheel: {
+//     sensitivity: 1,
+//   },
 
-  watchOverflow: true,
-  speed: 800,
-  observer: true,
-  observeParents: true,
-});
+//   watchOverflow: true,
+//   speed: 800,
+//   observer: true,
+//   observeParents: true,
+// });
 
 
 
