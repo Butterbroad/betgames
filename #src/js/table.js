9330fa9446f0713c82f648205106fc221a0a2316
Butterbroad <pyatochevskiy@gmail.com>
2020-12-04 17:51:50 +0300

    finished table

--- #src/js/table.js
+++ #src/js/table.js
@@ -1,7 +1,13 @@
 const url = 'https://maxline.by/api/TvBetLanding/getUsersForLanding?cors=1';
-
+const table = document.querySelector('.table');
 let data = fetch(url)
-  .then(response => response.json())
+  .then(response => {
+    if (response.ok) {
+      return response.json()
+    } else {
+      table.classList.add('empty');
+    }
+  })
   .then(item => {
     tableOne(item);
     tableTwo(item);
@@ -12,63 +18,72 @@ let data = fetch(url)
 //table 1
 function tableOne(item) {
   const firstTableItems = item.data[0].firstTable;
-  const firstTableWrapper = document.querySelector('.table__column_1');
   let imgSrc;
   let modClass;
-  const tableItems = firstTableItems.map(item => {
-    if (item.place === '1') {
-      imgSrc = './img/screen3/table1.svg';
-      modClass = 'table__item_1';
-    } else if (item.place === '2') {
-      imgSrc = './img/screen3/table2.svg';
-      modClass = 'table__item_2';
-    } else if (item.place === '3') {
-      imgSrc = './img/screen3/table3.svg';
-      modClass = 'table__item_3';
-    } else if (item.place === '4') {
-      imgSrc = './img/screen3/table4.svg';
-      modClass = 'table__item_4';
-    } else {
-      null
-    }
-    //make bet item
-    let tableItem = `
-    <div class="table__item ${modClass}">
-    <div class="table__item-num">
-      <img src="${imgSrc}" alt="">
-    </div>
-    <div class="table__item-id">
-      <div class="table__item-id-title">
-        № Счета
+  const tableColumn = document.querySelector('.table__column_1');
+
+  if (firstTableItems.length > 0) {
+    const firstTableWrapper = document.querySelector('.table__column_1');
+    const tableItems = firstTableItems.map(item => {
+      if (item.place === '1') {
+        imgSrc = './img/screen3/table1.svg';
+        modClass = 'table__item_1';
+      } else if (item.place === '2') {
+        imgSrc = './img/screen3/table2.svg';
+        modClass = 'table__item_2';
+      } else if (item.place === '3') {
+        imgSrc = './img/screen3/table3.svg';
+        modClass = 'table__item_3';
+      } else if (item.place === '4') {
+        imgSrc = './img/screen3/table4.svg';
+        modClass = 'table__item_4';
+      } else {
+        null
+      }
+      //make bet item
+      let tableItem = `
+      <div class="table__item ${modClass}">
+      <div class="table__item-num">
+        <img src="${imgSrc}" alt="">
       </div>
-      <div class="table__item-id-num">
-        ${item.id}
+      <div class="table__item-id">
+        <div class="table__item-id-title">
+          № Счета
+        </div>
+        <div class="table__item-id-num">
+          ${item.id}
+        </div>
       </div>
-    </div>
-    <div class="table__item-points">
-      <div class="table__item-points-title">
-        Очки
+      <div class="table__item-points">
+        <div class="table__item-points-title">
+          Очки
+        </div>
+        <div class="table__item-points-num">
+          ${item.points}
+        </div>
       </div>
-      <div class="table__item-points-num">
-        ${item.points}
+      <div class="table__item-win">
+          ${item.prize} BYN
       </div>
-    </div>
-    <div class="table__item-win">
-        ${item.prize} BYN
-    </div>
-  </div>`
-    return firstTableWrapper.innerHTML += tableItem;
-  });
+    </div>`
+      return firstTableWrapper.innerHTML += tableItem;
+    });
+  } else {
+    tableColumn.classList.add('empty');
+  }
 }
 
 //table 2
 function tableTwo(item) {
   const secondTableItems = item.data[0].secondTable.slice(0, 6);
-  const secondTableWrapper = document.querySelector('.table__column-wrapper_1');
-  const tableItems = secondTableItems.map(item => {
+  const tableColumn = document.querySelector('.table__column_2');
 
-    //make bet item
-    let tableItem = `
+  if (secondTableItems.length > 0) {
+    const secondTableWrapper = document.querySelector('.table__column-wrapper_1');
+    const tableItems = secondTableItems.map(item => {
+
+      //make bet item
+      let tableItem = `
     <div class="table__item">
     <div class="table__item-num">
       ${item.place}.
@@ -87,33 +102,42 @@ function tableTwo(item) {
       50 BYN
     </div>
   </div>`
-    return secondTableWrapper.innerHTML += tableItem;
-  });
+      return secondTableWrapper.innerHTML += tableItem;
+    });
+  } else {
+    tableColumn.classList.add('empty');
+  }
 }
 
 //table 3
 function tableThree(item) {
   const thirdTableItems = item.data[0].thirdTable.slice(0, 6);
-  const thirdTableWrapper = document.querySelector('.table__column-wrapper_2');
-  const tableItems = thirdTableItems.map(item => {
+  const tableColumn = document.querySelector('.table__column_3');
 
-    //make bet item
-    let tableItem = `
-    <div class="table__item">
-    <div class="table__item-num">
-      ${item.place}.
-    </div>
-    <div class="table__item-id">
-      <div class="table__item-id-num">
-      ${item.id}
+  if (thirdTableItems.length > 0) {
+    const thirdTableWrapper = document.querySelector('.table__column-wrapper_2');
+    const tableItems = thirdTableItems.map(item => {
+      //make bet item
+      let tableItem = `
+      <div class="table__item">
+      <div class="table__item-num">
+        ${item.place}.
       </div>
-    </div>
-    <div class="table__item-points">
-      <div class="table__item-points-num">
-      ${item.points}
+      <div class="table__item-id">
+        <div class="table__item-id-num">
+        ${item.id}
+        </div>
       </div>
-    </div>
-  </div>`
-    return thirdTableWrapper.innerHTML += tableItem;
-  });
+      <div class="table__item-points">
+        <div class="table__item-points-num">
+        ${item.points}
+        </div>
+      </div>
+    </div>`
+      return thirdTableWrapper.innerHTML += tableItem;
+    });
+  } else {
+    tableColumn.classList.add('empty');
+  }
+
 }
